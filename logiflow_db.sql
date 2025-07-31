7c1503dff5685ba836ad2d7dd297e0a787e87810
buzz9248 <buzz9248@naver.com>
2025-07-31 15:56:29 +0900

    feat: create-log-table

--- logiflow_db.sql
+++ logiflow_db.sql
@@ -47,11 +47,33 @@ CREATE TABLE IF NOT EXISTS `customers` (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     # 담당자 직책 직무 이름 연락처 이메일
+    charge_position VARCHAR(20),
+    charge_department VARCHAR(20),
+    charge_name VARCHAR(50),
+    charge_phone VARCHAR(20),
+    charge_email VARCHAR(100),
     
     CONSTRAINT uq_business_number UNIQUE (business_number),
     CONSTRAINT fk_customers_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT ck_customers_status CHECK (status IN ('PENDING', 'APPROVED', 'REJECTED'))
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 계약
+CREATE TABLE IF NOT EXISTS `contracts` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    customer_id BIGINT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    price INT NOT NULL,
+    volume_limit INT NOT NULL,
+    special_terms TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+	CONSTRAINT fk_contracts_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
+    CONSTRAINT ck_contracts_status CHECK (status IN ('PENDING', 'APPROVED', 'REJECTED', 'EXPIRED'))
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --# 기사
 CREATE TABLE IF NOT EXISTS `drivers` (
@@ -111,7 +133,8 @@ CREATE TABLE IF NOT EXISTS `driver_licenses` (
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     
     CONSTRAINT fk_driver_licenses_driver_id FOREIGN KEY (driver_id) REFERENCES drivers(id) ON DELETE CASCADE,
-    CONSTRAINT ck_driver_licenses_type CHECK (type IN ('', '', ''))
+    CONSTRAINT ck_driver_licenses_type CHECK (type IN ('CLASS1', 'CLASS2', 'HGV'))
+    # CLASS1: 1종 보통, CLASS2: 2종 보통, HGV: 1종 대형
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --# 운송수단
@@ -120,7 +143,8 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
   vehicle_number VARCHAR(255) NOT NULL,
   capacity INT NOT NULL,
   fuel VARCHAR(20) NOT NULL,
-  mileage FLOAT NOT NULL,
+  mileage DECIMAL(7,2) NOT NULL,
+  status VARCHAR(20) NOT NULL,
   model_name VARCHAR(20) NOT NULL,
   model_year YEAR NOT NULL,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -128,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
 
   CONSTRAINT uq_vehicles_number UNIQUE (vehicle_number),
   CONSTRAINT ck_vehicles_fuel CHECK (fuel IN ('GASOLINE', 'LPG', 'ELECTRIC', 'DIESEL')),
-  CONSTRAINT ck_vehicles_mileage CHECK (mileage IN ('AVAILABLE', 'IN_USE', 'UNDER_MAINTENANCE', 'INACTIVE'))
+  CONSTRAINT ck_vehicles_status CHECK (status IN ('AVAILABLE', 'IN_USE', 'UNDER_MAINTENANCE', 'INACTIVE'))
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --# 배송
@@ -137,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `deliveries` (
   customer_id BIGINT NOT NULL,
   request_date DATETIME NOT NULL,
   item VARCHAR(255) NOT NULL,
-  weight FLOAT NOT NULL,
+  weight DECIMAL(7,2) NOT NULL,
   message VARCHAR(255),
   status VARCHAR(20) NOT NULL,
 
@@ -157,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `deliveries` (
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
   CONSTRAINT fk_deliveries_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
-  CONSTRAINT ck_delivery_status CHECK (status IN ('REQUESTED', 'CANCELLED', 'ASSIGNED', 'REJECTED'))
+  CONSTRAINT ck_delivery_status CHECK (status IN ('REQUESTED', 'RECEIPTED', 'CANCELLED', 'ASSIGNED', 'REJECTED'))
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --# 배정
@@ -179,8 +203,8 @@ CREATE TABLE IF NOT EXISTS `allocations` (
   delivery_id BIGINT NOT NULL,
   assignment_id BIGINT NOT NULL,
   district_name VARCHAR(20) NOT NULL,
-  start_mileage FLOAT NOT NULL,
-  end_mileage FLOAT NOT NULL,
+  start_mileage DECIMAL(7,2) NOT NULL,
+  end_mileage DECIMAL(7,2) NOT NULL,
   status VARCHAR(20) NOT NULL,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -240,3 +264,234 @@ CREATE TABLE IF NOT EXISTS `alerts` (
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_alerts_driver_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE  
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 
+CREATE TABLE IF NOT EXISTS `delete_log` (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  table_name VARCHAR(50) NOT NULL,
+  record_id BIGINT NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  expired_at DATE NOT NULL,
+  deleted_by BIGINT NOT NULL,
+  delete_type VARCHAR(20) NOT NULL DEFAULT 'SOFT',
+  
+  CONSTRAINT fk_delete_log_deleted_by FOREIGN KEY (deleted_by) REFERENCES users(id) ON DELETE SET NULL,
+  CONSTRAINT ck_delete_log_delete_type CHECK (delete_type IN ('SOFT', 'HARD'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 고객사 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `customers_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    customer_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_customers_update_log_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE SET NULL,
+    CONSTRAINT fk_customers_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 고객사 상태 로그
+CREATE TABLE IF NOT EXISTS `customers_status_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	customer_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	prev_status VARCHAR(100) NOT NULL,
+	new_status VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       
+	CONSTRAINT fk_customers_status_log_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE SET NULL,
+    CONSTRAINT fk_customers_status_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL,
+	CONSTRAINT ck_customers_status_log_prev_status CHECK (prev_status IN ('PENDING', 'APPROVED', 'REJECTED')),
+	CONSTRAINT ck_customers_status_log_new_status CHECK (new_status IN ('PENDING', 'APPROVED', 'REJECTED'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 차량 상태 로그
+CREATE TABLE IF NOT EXISTS `vehicles_status_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	vehicle_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	prev_status VARCHAR(100) NOT NULL,
+	new_status VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       
+	CONSTRAINT fk_vehicles_status_log_vehicle_id FOREIGN KEY (vehicle_id) REFERENCES vehicles(id) ON DELETE SET NULL,
+	CONSTRAINT fk_vehicles_status_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL,
+	CONSTRAINT ck_vehicles_status_log_prev_status CHECK (prev_status IN ('AVAILABLE', 'IN_USE', 'UNDER_MAINTENANCE', 'INACTIVE')),
+	CONSTRAINT ck_vehicles_status_log_new_status CHECK (new_status IN ('AVAILABLE', 'IN_USE', 'UNDER_MAINTENANCE', 'INACTIVE'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 기사 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `drivers_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    driver_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_drivers_update_log_driver_id FOREIGN KEY (driver_id) REFERENCES drivers(id) ON DELETE SET NULL,
+    CONSTRAINT fk_drivers_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 기사 상태 로그
+CREATE TABLE IF NOT EXISTS `drivers_status_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	driver_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	prev_status VARCHAR(100) NOT NULL,
+	new_status VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       
+	CONSTRAINT fk_drivers_status_log_driver_id FOREIGN KEY (driver_id) REFERENCES drivers(id) ON DELETE SET NULL,
+	CONSTRAINT fk_drivers_status_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL,
+	CONSTRAINT ck_drivers_status_log_prev_status CHECK (prev_status IN ('ACTIVED', 'INACTIVED', 'ASSIGNED')),
+	CONSTRAINT ck_drivers_status_log_new_status CHECK (new_status IN ('ACTIVED', 'INACTIVED', 'ASSIGNED'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 직원 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `employees_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    employee_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_employees_update_log_employee_id FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employees_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 직원 인사 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `employees_org_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    employee_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_employees_org_log_employee_id FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employees_org_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 운전 면허증 수정 로그
+CREATE TABLE IF NOT EXISTS `driver_license_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    driver_license_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_driver_license_log_driver_license_id FOREIGN KEY (driver_license_id) REFERENCES driver_licenses(id) ON DELETE SET NULL,
+    CONSTRAINT fk_driver_license_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 배송 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `deliveries_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    delivery_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_deliveries_update_log_delivery_id FOREIGN KEY (delivery_id) REFERENCES deliveries(id) ON DELETE SET NULL,
+    CONSTRAINT fk_deliveries_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 배송 상태 로그
+CREATE TABLE IF NOT EXISTS `deliveries_status_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	delivery_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	prev_status VARCHAR(100) NOT NULL,
+	new_status VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       
+	CONSTRAINT fk_deliveries_status_log_delivery_id FOREIGN KEY (delivery_id) REFERENCES deliveries(id) ON DELETE SET NULL,
+	CONSTRAINT fk_deliveries_status_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL,
+	CONSTRAINT ck_deliveries_status_log_prev_status CHECK (prev_status IN ('REQUESTED', 'RECEIPTED', 'CANCELLED', 'ASSIGNED', 'REJECTED')),
+	CONSTRAINT ck_deliveries_status_log_new_status CHECK (new_status IN ('REQUESTED', 'RECEIPTED', 'CANCELLED', 'ASSIGNED', 'REJECTED'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 배정 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `assignments_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    assignment_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_assignments_update_log_assignment_id FOREIGN KEY (assignment_id) REFERENCES assignments(id) ON DELETE SET NULL,
+    CONSTRAINT fk_assignments_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 배차 정보 수정 로그
+CREATE TABLE IF NOT EXISTS `allocations_update_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    allocation_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	type VARCHAR(50) NOT NULL,
+    prev_data VARCHAR(100),
+    new_data VARCHAR(100),
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_allocations_update_log_allocation_id FOREIGN KEY (allocation_id) REFERENCES allocations(id) ON DELETE SET NULL,
+    CONSTRAINT fk_allocations_update_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--# 배차 상태 로그
+CREATE TABLE IF NOT EXISTS `allocations_status_log` (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	allocation_id BIGINT,
+    changed_by BIGINT,
+    changed_by_username VARCHAR(20) NOT NULL,
+    change_reason VARCHAR(255),
+	prev_status VARCHAR(100) NOT NULL,
+	new_status VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	
+	CONSTRAINT fk_allocations_status_log_allocation_id FOREIGN KEY (allocation_id) REFERENCES allocations(id) ON DELETE SET NULL,
+	CONSTRAINT fk_allocations_status_log_changed_by FOREIGN KEY (changed_by) REFERENCES users(id) ON DELETE SET NULL,
+	CONSTRAINT ck_allocations_status_log_prev_status CHECK (prev_status IN ('ASSIGNED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED')),
+	CONSTRAINT ck_allocations_status_log_new_status CHECK (new_status IN ('ASSIGNED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED'))
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+
+
